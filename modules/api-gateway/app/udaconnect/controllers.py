7f3ea80273d00b450b363dd2dc008c47327d5942
grushler <girishsortur@gmail.com>
2021-11-21 17:08:31 +0530

    Updating connection resource

--- modules/api-gateway/app/udaconnect/controllers.py
+++ modules/api-gateway/app/udaconnect/controllers.py
@@ -75,11 +75,9 @@ class LocationResource(Resource):
 class ConnectionDataResource(Resource):
     @responds(schema=ConnectionSchema, many=True)
     def get(self, person_id) -> ConnectionSchema:
-        # start_date: datetime = datetime.strptime(
-        #     request.args["start_date"], DATE_FORMAT
-        # )
-        # end_date: datetime = datetime.strptime(request.args["end_date"], DATE_FORMAT)
-        # distance: Optional[int] = request.args.get("distance", 5)
+        start_date: datetime = request.args["start_date"]
+        end_date: datetime = request.args["end_date"]
+        distance: Optional[int] = request.args.get("distance", 5)
 
         results = ConnectionService.find_contacts(
             person_id=person_id,

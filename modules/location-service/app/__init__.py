6952daa913747c2833352c03501cf2bfc6f53b91
grushler <girishsortur@gmail.com>
2021-11-21 16:15:38 +0530

    Changing few steps for kafka service

--- modules/location-service/app/__init__.py
+++ modules/location-service/app/__init__.py
@@ -26,9 +26,7 @@ def create_app(env=None):
         KAFKA_SERVER = 'kafka-0.kafka-headless.default.svc.cluster.local:9093'
         producer = KafkaProducer(bootstrap_servers = KAFKA_SERVER, value_serializer=lambda v: json.dumps(v).encode('utf-8'))
         consumer = KafkaConsumer(
-            bootstrap_servers = KAFKA_SERVER, 
-            auto_offset_reset='earliest',
-            group_id='kafka-group',
+            bootstrap_servers = KAFKA_SERVER,
             value_deserializer=lambda m: json.loads(m.decode('utf-8')))
         g.kafka_producer = producer
         g.kafka_consumer = consumer

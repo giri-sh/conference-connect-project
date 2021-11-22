c24c0bfe6ca52e5b39ebd27fc2dc3a049a939ba9
grushler <girishsortur@gmail.com>
2021-11-22 15:08:05 +0530

    Updating kafka changes

--- modules/location-service/app/__init__.py
+++ modules/location-service/app/__init__.py
@@ -13,6 +13,7 @@ logger = logging.getLogger("loc-service-api")
 
 db = SQLAlchemy()
 
+
 def create_app(env=None):
     from app.config import config_by_name
     from app.routes import register_routes
@@ -26,31 +27,35 @@ def create_app(env=None):
     register_routes(api, app)
     db.init_app(app)
 
-    @app.before_request
-    def before_request():
-        KAFKA_SERVER = 'kafka-0.kafka-headless.default.svc.cluster.local:9093'
-        # producer = KafkaProducer(bootstrap_servers = KAFKA_SERVER, value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-        consumer = KafkaConsumer(bootstrap_servers = KAFKA_SERVER, value_deserializer=lambda m: json.loads(m.decode('utf-8')))
-        # g.kafka_producer = producer
-        # g.kafka_consumer = consumer
-        # consumer = g.kafka_consumer
-        TOPIC_NAME = 'location_topics'
-        tp = TopicPartition(TOPIC_NAME, 0)
-        consumer.assign([tp])
-        lastOffset = consumer.position(tp)
-        consumer.seek_to_beginning(tp)
+    KAFKA_SERVER = 'kafka-0.kafka-headless.default.svc.cluster.local:9093'
+    # producer = KafkaProducer(bootstrap_servers = KAFKA_SERVER, value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+    consumer = KafkaConsumer(bootstrap_servers=KAFKA_SERVER,
+                             value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+    # g.kafka_producer = producer
+    # g.kafka_consumer = consumer
+    # consumer = g.kafka_consumer
+    TOPIC_NAME = 'location_topics'
+    logger.info("Calling consumer to consume the message")
+    tp = TopicPartition(TOPIC_NAME, 0)
+    consumer.assign([tp])
+    lastOffset = consumer.position(tp)
+    consumer.seek_to_beginning(tp)
 
-        for message in consumer:
-            new_location = Location()
-            data = message.value
-            logger.info(data)
-            new_location.person_id = data["person_id"]
-            new_location.creation_time = data["creation_time"]
-            new_location.coordinate = ST_Point(data["latitude"], data["longitude"])
-            db.session.add(new_location)
-            db.session.commit()
-            if message.offset == lastOffset - 1:
-                break
+    for message in consumer:
+        new_location = Location()
+        data = message.value
+        logger.info(data)
+        new_location.person_id = data["person_id"]
+        new_location.creation_time = data["creation_time"]
+        new_location.coordinate = ST_Point(
+            data["latitude"], data["longitude"])
+        db.session.add(new_location)
+        db.session.commit()
+        if message.offset == lastOffset - 1:
+            break
+
+    # @app.before_request
+    # def before_request():
 
     @app.route("/health")
     def health():

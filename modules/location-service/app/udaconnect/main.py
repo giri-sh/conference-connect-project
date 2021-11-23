e4674bccafa40ceb9ee56c53528ca0f3162a62b9
grushler <girishsortur@gmail.com>
2021-11-23 12:25:54 +0530

    New changes to separate kafka service

--- modules/location-service/app/udaconnect/main.py
+++ modules/location-service/app/udaconnect/main.py
@@ -21,7 +21,7 @@ logger = logging.getLogger("location-service")
 TOPIC_NAME = 'location_topic'
 KAFKA_SERVER = 'kafka-0.kafka-headless.default.svc.cluster.local:9093'
 
-consumer = KafkaConsumer(bootstrap_servers=KAFKA_SERVER, group_id='test-group', auto_commit_interval_ms=2000,
+consumer = KafkaConsumer(bootstrap_servers=KAFKA_SERVER, group_id='test-group',
                          value_deserializer=lambda m: json.loads(m.decode('utf-8')))
 
 logger.info("Calling consumer to consume the message")
@@ -29,22 +29,22 @@ tp = TopicPartition(TOPIC_NAME, 0)
 consumer.assign([tp])
 lastOffset = consumer.position(tp)
 consumer.seek_to_beginning(tp)
+consumer.poll(10)
 
-while True:
-    for message in consumer:
-        logger.info(message)
-        if message.offset == 0:
-            continue
-        new_location = Location()
-        data = message.value
-        logger.info(data)
-        new_location.person_id = data["person_id"]
-        new_location.creation_time = data["creation_time"]
-        new_location.coordinate = ST_Point(
-            data["latitude"], data["longitude"])
-        db_ops.save_location_data(new_location)
-        if message.offset == lastOffset - 1:
-            break
+for message in consumer:
+    logger.info(message)
+    if message.offset == 0:
+        continue
+    new_location = Location()
+    data = message.value
+    logger.info(data)
+    new_location.person_id = data["person_id"]
+    new_location.creation_time = data["creation_time"]
+    new_location.coordinate = ST_Point(
+        data["latitude"], data["longitude"])
+    db_ops.save_location_data(new_location)
+    if message.offset == lastOffset - 1:
+        break
 
 
 # TOPIC_NAME = 'location_topic'

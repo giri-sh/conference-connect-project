2516766b75b6ba5d9667cc620bb340de408f7a7e
grushler <girishsortur@gmail.com>
2021-11-23 12:33:17 +0530

    Cleaning up the service

--- modules/location-service/app/udaconnect/__init__.py
+++ modules/location-service/app/udaconnect/__init__.py
@@ -1,8 +1,8 @@
-from app.udaconnect.models import Location, Person
-from app.udaconnect.schemas import LocationSchema
+# from app.udaconnect.models import Location, Person
+# from app.udaconnect.schemas import LocationSchema
 
 
-def register_routes(api, app, root="api"):
-    from app.udaconnect.controllers import api as loc_service_api
+# def register_routes(api, app, root="api"):
+#     from app.udaconnect.controllers import api as loc_service_api
 
-    api.add_namespace(loc_service_api, path=f"/{root}")
+#     api.add_namespace(loc_service_api, path=f"/{root}")
--- modules/location-service/app/udaconnect/services.py
+++ modules/location-service/app/udaconnect/services.py
@@ -1,36 +1,36 @@
-import logging
-from datetime import datetime, timedelta
-from typing import Dict, List
-import json
+# import logging
+# from datetime import datetime, timedelta
+# from typing import Dict, List
+# import json
 
-from app import db
-from app.udaconnect.models import Location, Person
-from app.udaconnect.schemas import LocationSchema
-from geoalchemy2.functions import ST_AsText, ST_Point
-from sqlalchemy.sql import text
-from flask import g
-from kafka import TopicPartition, KafkaProducer, KafkaConsumer
+# from app import db
+# from app.udaconnect.models import Location, Person
+# from app.udaconnect.schemas import LocationSchema
+# from geoalchemy2.functions import ST_AsText, ST_Point
+# from sqlalchemy.sql import text
+# from flask import g
+# from kafka import TopicPartition, KafkaProducer, KafkaConsumer
 
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger("loc-service-api")
+# logging.basicConfig(level=logging.DEBUG)
+# logger = logging.getLogger("loc-service-api")
 
-DATE_FORMAT = "%Y-%m-%dT%H:%M:%S"
+# DATE_FORMAT = "%Y-%m-%dT%H:%M:%S"
 
 
-class LocationService:
-    @staticmethod
-    def retrieve(location_id) -> Location:
-        location, coord_text = (
-            db.session.query(Location, Location.coordinate.ST_AsText())
-            .filter(Location.id == location_id)
-            .one()
-        )
+# class LocationService:
+#     @staticmethod
+#     def retrieve(location_id) -> Location:
+#         location, coord_text = (
+#             db.session.query(Location, Location.coordinate.ST_AsText())
+#             .filter(Location.id == location_id)
+#             .one()
+#         )
 
-        # Rely on database to return text form of point to reduce overhead of conversion in app code
-        location.wkt_shape = coord_text
-        return location
+#         # Rely on database to return text form of point to reduce overhead of conversion in app code
+#         location.wkt_shape = coord_text
+#         return location
 
-    # @staticmethod
-    # def create(location: Dict) -> Location:
-    #     return location
+#     # @staticmethod
+#     # def create(location: Dict) -> Location:
+#     #     return location
 
